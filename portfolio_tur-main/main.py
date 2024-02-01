93b7ad45ec256a7991aa650a2bd3f1245ba158a6
N.Efe Güner <necmettinefeguner6@gmail.com>
2024-02-01 12:24:12 +0300

    vghhdhdhdhdhh

--- portfolio_tur-main/main.py
+++ portfolio_tur-main/main.py
@@ -15,9 +15,22 @@ def index():
 @app.route('/', methods=['POST'])
 def process_form():
     button_python = request.form.get('button_python')
-    return render_template('index.html', button_python=button_python)
+    button_discord=request.form.get('button_discord')
+    button_html=request.form.get('button_html')
+    button_db=request.form.get('button_db')
+    return render_template('index.html',
+                            button_python=button_python,
+                            button_discord=button_discord,
+                            button_html=button_html,
+                            button_db=button_db)
+@app.route('/')
+def feedback():
+    email=request.form.get('email')
+    text=request.form.get('text')
+    return render_template('index.html',
+                           email=email,
+                           text=text)
 
-@app.route('/', methods=[''])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
